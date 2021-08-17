e938471406d2594e2058354925e9d955c808753d
robg245 <88699403+robg245@users.noreply.github.com>
2021-08-16 19:44:19 -0700

    Basic HTML and JavaScript_1 folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,8 @@
+<script>
+	function My_first_Function()	{
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+	</script>
